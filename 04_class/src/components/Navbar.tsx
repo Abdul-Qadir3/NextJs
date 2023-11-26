c10fa6eef488dfd7dc94058e7619cd11a7870f4f
Abdul-Qadir3 <aqadirsamdani@gmail.com>
2023-11-26 22:51:21 +0500

    new addition

---
+++ 04_class/src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+    // rfce will give boiler plate
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <div>
+            {/* logo */}
+            <div></div>
+            {/* Menu */}
+            <div></div>
+            {/* Avatar */}
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
